c172ab20507a5ae9789d7f000cb227ae20e75c17
paulayo93 <52790286+paulayo93@users.noreply.github.com>
2025-02-05 04:38:13 +0100

    add product feature

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,6 +8,7 @@ exports.createProduct = async (req, res) => {
 
         return res.status(422).json({error: 'Name is required'})
         }
+
         if(!req.body.price) {
             
             return res.status(422).json({error: 'Price is required'})
@@ -17,6 +18,25 @@ exports.createProduct = async (req, res) => {
                 return res.status(422).json({error: 'Price must be a non-negative number'})  
             }
         }
+
+        if(!req.body.categoryId) {
+            
+            return res.status(422).json({error: 'Category id is required'})
+
+        } else {
+
+            if(!await prisma.category.findUnique({where: {id: parseInt(req.body.categoryId) }})) {
+            
+                return  res.status(404).json({error: 'Category id not found'})
+            }
+        }
+
+        const newProduct = await prisma.product.create({
+        
+            data: req.body
+        })
+
+        return res.status(500).json(newProduct)
 
     } catch(error) {
 
